464037e014176f09ee5385756250e96026ecf64f
Ishimoto Shinobu <nagakamira@gmail.com>
2021-12-13 22:59:52 +0300

    finish mem* and wmem* functions
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- libc/src/string.rs
+++ libc/src/string.rs
@@ -1,8 +1,64 @@
 use core::ffi::c_void;
+use core::ptr::null_mut;
 
 use crate::basic_types::c_int;
 use crate::basic_types::c_char;
+use crate::basic_types::c_uchar;
 use crate::basic_types::size_t;
+
+#[no_mangle]
+pub extern "C" fn memchr(src: *const c_void, c: c_int, n: size_t) -> *mut c_void {
+	let src1: *const c_char = src as *const c_char;
+	let mut i = n;
+
+	unsafe {
+		while i > 0 {
+			i -= 1;
+			if *src1.add(i) == (c as c_char) {
+				return src1.add(i) as *mut c_void;
+			}
+		}
+	}
+
+	return null_mut();
+}
+
+#[no_mangle]
+pub extern "C" fn memcmp(cx: *const c_void, ct: *const c_void, n: size_t) -> c_int {
+	let cx1: *const c_uchar = cx as *const c_uchar;
+	let ct1: *const c_uchar = ct as *const c_uchar;
+	let mut i = n;
+
+	while i > 0 {
+		i -= 1;
+		unsafe {
+			let cx2: c_uchar = *(cx1).add(i);
+			let ct2: c_uchar = *(ct1).add(i);
+
+			if cx2 != ct2 {
+				return (cx2 - ct2).into();
+			}
+		}
+	}
+
+	return 0;
+}
+
+// Adopted from Redox OS
+#[no_mangle]
+pub extern "C" fn ata_memccpy(dest: *mut c_void, src: *const c_void, c: c_int, n: size_t) -> *mut c_void {
+	let to = memchr(src, c, n);
+	if to.is_null() {
+		return to;
+	}
+
+	let dist = (to as usize) - (src as usize);
+	if memcpy(dest, src, dist).is_null() {
+		return null_mut();
+	}
+
+	unsafe { return (dest as *mut u8).add(dist + 1) as *mut c_void; }
+}
 
 #[no_mangle]
 pub extern "C" fn memcpy(dest: *mut c_void, src: *const c_void, n: size_t) -> *mut c_void {
--- libc/src/wchar.rs
+++ libc/src/wchar.rs
@@ -1,6 +1,45 @@
+use core::ptr::null_mut;
+
 use crate::basic_types::c_int;
 use crate::basic_types::size_t;
 use crate::basic_types::wchar_t;
+
+#[no_mangle]
+pub extern "C" fn wmemchr(src: *const wchar_t, c: wchar_t, n: size_t) -> *mut wchar_t {
+	let mut i = n;
+
+	unsafe {
+		while i > 0 {
+			i -= 1;
+			if *src.add(i) == c {
+				return src.add(i) as *mut wchar_t;
+			}
+		}
+	}
+
+	return null_mut();
+}
+
+#[no_mangle]
+pub extern "C" fn wmemcmp(cx: *const wchar_t, ct: *const wchar_t, n: size_t) -> c_int {
+	let cx1: *const wchar_t = cx as *const wchar_t;
+	let ct1: *const wchar_t = ct as *const wchar_t;
+	let mut i = n;
+
+	while i > 0 {
+		i -= 1;
+		unsafe {
+			let cx2: wchar_t = *(cx1).add(i);
+			let ct2: wchar_t = *(ct1).add(i);
+
+			if cx2 != ct2 {
+				return (cx2 - ct2).into();
+			}
+		}
+	}
+
+	return 0;
+}
 
 #[no_mangle]
 pub extern "C" fn wmemcpy(dest: *mut wchar_t, src: *const wchar_t, n: size_t) -> *mut wchar_t {
